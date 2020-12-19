9a7fbe14af21c0d1766e1abce47534be612870ff
Alex Smith <alex.vesson.smith@gmail.com>
2020-12-19 19:23:15 +0300

    task done

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,55 @@
+import sortCharacter from '../index';
+
+test ('sort charaters1', () => {
+  const characters = [
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80}
+  ];
+
+  const expected = [
+    {name: 'мечник', health: 10},
+    {name: 'лучник', health: 80},
+    {name: 'маг', health: 100}
+  ];
+
+  const recieved = sortCharacter(characters);
+
+  expect(recieved).toEqual(expected);
+});
+
+test ('sort charaters2', () => {
+  const characters = [
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10},
+    {name: 'маг', health: 100}
+  ];
+
+  const expected = [
+    {name: 'мечник', health: 10},
+    {name: 'лучник', health: 80},
+    {name: 'маг', health: 100},
+  ];
+
+  const recieved = sortCharacter(characters);
+
+  expect(recieved).toEqual(expected);
+});
+
+test ('sort charaters3', () => {
+  const characters = [
+    {name: 'маг', health: 100},
+    {name: 'лучник', health: 80},
+    {name: 'мечник', health: 10}
+  ];
+
+  const expected = [
+    {name: 'мечник', health: 10},
+    {name: 'лучник', health: 80},
+    {name: 'маг', health: 100},
+  ];
+
+  const recieved = sortCharacter(characters);
+
+  expect(recieved).toEqual(expected);
+});
---
+++ src/index.js
@@ -0,0 +1,4 @@
+export default function sortCharacter(characters){
+  return characters.sort((a, b) => parseFloat(a.health) - parseFloat(b.health));
+}
+
